3fc38855fe28ccf5358ba9d8340848ebd4c3ee67
fmotzet <felix.motzet@gmail.com>
2026-03-11 15:23:02 +0100

    from location

--- home/home-TP-p15v.nix
+++ home/home-TP-p15v.nix
@@ -5,10 +5,12 @@
     #./keepassxc.nix
   ];
 
-  wayland.windowManager.hyprland.settings = {
-    exec-once = [
-      "firefox -new-tab -url https://zabbix.muc.boerse-go.de/ -new-tab -url https://outlook.office.com/ -new-tab -url https://stock3.atlassian.net/jira/your-work -new-tab -url https://monkeytype.com/ -new-tab -url https://open.spotify.com/ -new-tab -url http://bgtop.dc1.boerse-go.de/snapshots.php?autoRefresh=60&limit=500"
-      "teams-for-linux"
-    ];
+  home-manager.users.felix = {
+    wayland.windowManager.hyprland.settings = {
+      exec-once = [
+        "firefox -new-tab -url https://zabbix.muc.boerse-go.de/ -new-tab -url https://outlook.office.com/ -new-tab -url https://stock3.atlassian.net/jira/your-work -new-tab -url https://monkeytype.com/ -new-tab -url https://open.spotify.com/ -new-tab -url http://bgtop.dc1.boerse-go.de/snapshots.php?autoRefresh=60&limit=500"
+        "teams-for-linux"
+      ];
+    };
   };
 }
--- home/home-TP-t14g6.nix
+++ home/home-TP-t14g6.nix
@@ -5,10 +5,12 @@
     #./keepassxc.nix
   ];
 
-  wayland.windowManager.hyprland.settings = {
-    exec-once = [
-      "brave https://zabbix.muc.boerse-go.de/ https://outlook.office.com/ https://stock3.atlassian.net/jira/your-work https://monkeytype.com/ https://open.spotify.com/ http://bgtop.dc1.boerse-go.de/snapshots.php?autoRefresh=60&limit=500"
-      "teams-for-linux"
-    ];
+  home-manager.users.felix = {
+     wayland.windowManager.hyprland.settings = {
+      exec-once = [
+        "brave https://zabbix.muc.boerse-go.de/ https://outlook.office.com/ https://stock3.atlassian.net/jira/your-work https://monkeytype.com/ https://open.spotify.com/ http://bgtop.dc1.boerse-go.de/snapshots.php?autoRefresh=60&limit=500"
+        "teams-for-linux"
+      ];
+    };
   };
 }
