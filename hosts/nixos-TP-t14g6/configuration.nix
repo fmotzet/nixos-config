194c4139607be59c17ab5b60ad176a1c3df5ac76
fmotzet <felix.motzet@gmail.com>
2026-03-11 15:26:27 +0100

    fixed xdg.portal warning

--- hosts/nixos-TP-t14g6/configuration.nix
+++ hosts/nixos-TP-t14g6/configuration.nix
@@ -40,6 +40,9 @@
     xwayland.enable = true;
   };
 
+  # XDG portal config (required since xdg-desktop-portal 1.17)
+  xdg.portal.config.common.default = "*";
+
   # Enable hyprlock for PAM authentication
   programs.hyprlock.enable = true;
 
--- hosts/nixos-fw13/configuration.nix
+++ hosts/nixos-fw13/configuration.nix
@@ -42,6 +42,9 @@
     enable = true;
     xwayland.enable = true;
   };
+
+  # XDG portal config (required since xdg-desktop-portal 1.17)
+  xdg.portal.config.common.default = "*";
   # Enable hyprlock for PAM authentication
   programs.hyprlock.enable = true;
 
