48ac4696ae40fed1f2f18ab5bd95867b202a0d01
fmotzet <felix.motzet@gmail.com>
2026-03-19 21:21:18 +0100

    added nixos nextcloud vm

--- flake.nix
+++ flake.nix
@@ -47,5 +47,13 @@
         }
       ];
     };
+
+    # VMs
+    nixosConfigurations.mnextcloud01 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/vms/mnextcloud01/configuration.nix
+      ];
+    };
   };
 }
---
+++ hosts/vms/mnextcloud01/configuration.nix
@@ -0,0 +1,75 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../../system/users.nix
+  ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+
+  networking.hostName = "mnextcloud01";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Berlin";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console.keyMap = "de";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    nfs-utils
+    htop
+  ];
+
+  # NFS mount for Nextcloud data
+  fileSystems."/mnt/nextcloud-data" = {
+    device = "192.168.178.128:/srv/nfs/shared/nextcloud";
+    fsType = "nfs";
+    options = [
+      "nfsvers=3"
+      "soft"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=600"
+      "noauto"
+    ];
+  };
+
+  # Nextcloud
+  # Before first activation, create the admin password file:
+  #   echo -n "your-password" > /etc/nextcloud-admin-pass
+  #   chmod 400 /etc/nextcloud-admin-pass
+  services.nextcloud = {
+    enable = true;
+    package = pkgs.nextcloud30;
+    hostName = "mnextcloud01";
+    datadir = "/mnt/nextcloud-data";
+    https = false;
+    configureRedis = true;
+    database.createLocally = true;
+    config = {
+      dbtype = "pgsql";
+      adminpassFile = "/etc/nextcloud-admin-pass";
+    };
+    settings = {
+      overwriteprotocol = "http";
+      default_phone_region = "DE";
+    };
+  };
+
+  # Open HTTP port
+  networking.firewall.allowedTCPPorts = [ 80 ];
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+  };
+
+  system.stateVersion = "25.11";
+}
