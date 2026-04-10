82772451a6be44a8354324653df8720cf1217827
fmotzet <felix.motzet@gmail.com>
2026-04-10 17:09:33 +0200

    swaync color fix

--- home/swaync.nix
+++ home/swaync.nix
@@ -49,7 +49,7 @@
       }
 
       .control-center {
-        background: rgba(20, 20, 35, 0.9);
+        background: rgba(255, 255, 255, 0.9);
         border-radius: 12px;
         border: 1px solid rgba(255, 255, 255, 0.2);
         color: #ffffff;
