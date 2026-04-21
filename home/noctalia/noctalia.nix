34136c44994af28e4b936417d2178e32d6cb8fa7
fmotzet <felix.motzet@gmail.com>
2026-04-21 15:52:51 +0200

    noctalia remove color through nix

--- home/noctalia/noctalia.nix
+++ home/noctalia/noctalia.nix
@@ -26,23 +26,23 @@
     # "m*" = background/accent colors, "mOn*" = foreground/text on top
     # Surfaces are white (opacity settings make them translucent)
     # Accents use light gray so indicators/bars don't disappear
-    colors = {
-      mPrimary = "#b3b3b3";
-      mOnPrimary = "#ffffff";
-      mSecondary = "#a0a0a0";
-      mOnSecondary = "#ffffff";
-      mTertiary = "#c8c8c8";
-      mOnTertiary = "#ffffff";
-      mError = "#ff5050";
-      mOnError = "#ffffff";
-      mSurface = "#ffffff";
-      mOnSurface = "#ffffff";
-      mSurfaceVariant = "#e6e6e6";
-      mOnSurfaceVariant = "#b3b3b3";
-      mHover = "#d0d0d0";
-      mOnHover = "#ffffff";
-      mOutline = "#cccccc";
-      mShadow = "#000000";
-    };
+    # colors = {
+    #   mPrimary = "#b3b3b3";
+    #   mOnPrimary = "#ffffff";
+    #   mSecondary = "#a0a0a0";
+    #   mOnSecondary = "#ffffff";
+    #   mTertiary = "#c8c8c8";
+    #   mOnTertiary = "#ffffff";
+    #   mError = "#ff5050";
+    #   mOnError = "#ffffff";
+    #   mSurface = "#ffffff";
+    #   mOnSurface = "#ffffff";
+    #   mSurfaceVariant = "#e6e6e6";
+    #   mOnSurfaceVariant = "#b3b3b3";
+    #   mHover = "#d0d0d0";
+    #   mOnHover = "#ffffff";
+    #   mOutline = "#cccccc";
+    #   mShadow = "#000000";
+    # };
   };
 }
