d3e415311cd8d6216ba0d5a2fd800f9b3df7e916
fmotzet <felix.motzet@gmail.com>
2026-05-04 16:53:45 +0200

    add memtest to figure out btrfs corruption errors

--- hosts/nixos-TP-t14g6/configuration.nix
+++ hosts/nixos-TP-t14g6/configuration.nix
@@ -11,6 +11,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.memtest86.enable = true;
   
   # Systemd boot opotimization
   # systemd.services.NetworkManager-wait-online.enable = false;
