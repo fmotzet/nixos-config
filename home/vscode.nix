6cd54e16e54c3fae03c8408dcedb4ec370534a79
fmotzet <felix.motzet@gmail.com>
2026-06-15 12:57:05 +0200

    fix vscode hm options

--- home/vscode.nix
+++ home/vscode.nix
@@ -2,16 +2,27 @@
 {
   programs.vscode = {
     enable = true;
-    profiles.default.userSettings = {
-      "[nix]"."editor.tabSize" = 2;
-      "editor.fontSize" = 14;
-      "workbench.sideBar.location" = "right";
-      "git.autofetch" = true;
-      "enableExtensionUpdateCheck" = true;
-      "workbench.colorTheme" = "Default Dark Modern";
+    profiles.default = {
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
+
+      userSettings = {
+        "[nix]"."editor.tabSize" = 2;
+        "editor.fontSize" = 14;
+        "workbench.sideBar.location" = "right";
+        "git.autofetch" = true;
+        "workbench.colorTheme" = "Default Dark Modern";
+
+        "extensions.autoUpdate" = false;
+        "telemetry.telemetryLevel" = "off";
+        "update.showReleaseNotes" = false;
+        "workbench.enableExperiments" = false;
+        "workbench.settings.enableNaturalLanguageSearch" = false;
+      };
+
+      extensions = with pkgs.vscode-extensions; [
+        anthropic.claude-code
+      ];
     };
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      anthropic.claude-code
-    ];
   };
 }
