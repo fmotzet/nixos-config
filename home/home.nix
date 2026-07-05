065642ad6fe914e1dec88b19aef182a4b98d1578
fmotzet <felix.motzet@gmail.com>
2026-07-05 18:44:04 +0200

    test

--- home/home.nix
+++ home/home.nix
@@ -42,7 +42,7 @@
         nix-dev-init = "cp /home/felix/nixos-config/templates/rust/flake.nix . && echo 'use flake' > .envrc && git add flake.nix .envrc && direnv allow";
         shsh = "start-hyprland";
         # is this cool?
-        tpwgen = "nix-shell -p pwgen --run 'pwgen 16'";
+        tpwgen = "nix-shell -p pwgen --run 'pwgen'";
       };
       initExtra = ''
         if [ "$TERM" = "xterm-kitty" ]; then
