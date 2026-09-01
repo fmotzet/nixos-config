373e9832142dab18108d803c89e320253968d20a
fmotzet <felix.motzet@gmail.com>
2026-09-01 20:52:48 +0200

    add automatic gc to vms

--- hosts/vms/shared.nix
+++ hosts/vms/shared.nix
@@ -5,4 +5,12 @@
     bind '"\e[5~": history-search-backward'
     bind '"\e[6~": history-search-forward'
   '';
+
+  # as i give vms small disks i have to make them run the garbage collector more automatically
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+  nix.settings.auto-optimise-store = true;
 }
